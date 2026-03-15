179b37c6380d1f479a4e9f8a86ac472108fa845a
Ayush Thapa <ayush@example.com>
2026-03-15 01:20:07 -0400

    Fix: Revert problematic manifest storage - use default WhiteNoise

--- resort_website/settings.py
+++ resort_website/settings.py
@@ -151,11 +151,6 @@ else:
     # Development: Local directory
     MEDIA_ROOT = BASE_DIR / 'media'
 
-# WhiteNoise staticfiles storage with media file support
-if 'DATABASE_URL' in os.environ:
-    # Production: Collect media files for WhiteNoise caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
